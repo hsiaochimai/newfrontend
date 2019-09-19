cbd7ea48a0bfdd2d481040ae107b26138eb5169a
hsiaochimai <jchmai21@gmail.com>
2019-09-19 08:26:10 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import {
   Switch,
   Route
 } from 'react-router-dom';
-import Home from '../src/components/home/Home';
-import Header from '../src/components/header/Header';
-import Contact from '../src/components/contact/contact';
+import Home from './components/home/Home';
+import Header from './components/header/Header';
+import Contact from './components/contact/contact';
 
-import Details from '../src/components/Details/Details'
-import Map from '../src/components/map/map';
+import Details from './components/Details/Details'
+import Map from './components/map/map';
 
 import './App.css';
 
--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import data from  '../src/services/data'
+import data from  './services/data'
 import "typeface-roboto";
 import "typeface-signika";
 import "typeface-righteous";
-import newData from  '../src/services/newData'
+import newData from  './services/newData'
 ReactDOM.render(<App DATA={newData}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
